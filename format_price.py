1da89c2074f8f4ac96df50429f1a54ff74a352f8
Dmitry Plevkov <wwarne@gmail.com>
2017-08-22 12:29:35 +0300

    Price formatting tool

--- format_price.py
+++ format_price.py
@@ -1,5 +1,24 @@
+import argparse
+import re
+
+
 def format_price(price):
-    pass
+    try:
+        price = float(price)
+    except (ValueError, TypeError):
+        return None
+
+    price = '{:,.2f}'.format(price).replace(',', ' ')
+    if ('-' in price) or ('n' in price) or ('i' in price):
+        # `-` for <0, `n` for nan,  `i` for infinity
+        return None
+    ending_zeros = re.search(r'[.]0+', price)
+    if ending_zeros:
+        price = price[:ending_zeros.start()]
+    return price
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser(description='Price string formatting tool')
+    parser.add_argument('price', help='Price to format')
+    parameters = parser.parse_args()
